d89d9ed919a6742cba588f0551662367ac00a19c
melonTai <marufuji25@gmail.com>
2021-08-14 15:47:01 +0900

    fix

--- mypackage/page/shutuba.py
+++ mypackage/page/shutuba.py
@@ -1,19 +1,7 @@
 from .base import BasePage
 from selenium.webdriver.remote.webelement import WebElement
 import bs4
-
-class HorseShutuba():
-    def __init__(self, element : bs4.element, param_keys : list):
-        # .HorseList tr からtdを検索
-        param_element_list = element.select("td")
-        # 印欄は不必要につき削除
-        del param_element_list[2]
-        del param_keys[2]
-        # 抽出値
-        param_values = [element.text for element in param_element_list]
-        # 属性に格納
-        self.params = dict(zip(param_keys, param_values))
-
+import re
 
 class ShutubaPage(BasePage):
     """RaceResultPage action methods come here.
@@ -24,13 +12,52 @@ class ShutubaPage(BasePage):
         cur_url = self.driver.current_url
         return "race/shutuba.html" in cur_url
 
+    def __get_horse_id(self, element: bs4.element):
+        """馬名が入ったtd要素からhorse_idを抽出する
+
+        Args:
+            element (WebElement): td要素
+        """
+        atag_elements = element.select("a")
+        if atag_elements:
+            atag_element = atag_elements[0]
+            url = atag_element.attrs["href"]
+            pattern = "horse/(\d*)"
+            match = re.findall(pattern, url)
+            if match:
+                horse_id = match[0]
+                return horse_id
+
+        return None
+        
+    def __get_jockey_id(self, element:bs4.element):
+        atag_elements = element.select("a")
+        if atag_elements:
+            atag_element = atag_elements[0]
+            url = atag_element.attrs["href"]
+            pattern = "jockey/(\d*)"
+            match = re.findall(pattern, url)
+            if match:
+                jockey_id = match[0]
+                return jockey_id
+        return None
+
     def get_horse_list(self):
         head_elements = self.soup.select(self.heads_locator)
-        heads = [element.get_text().replace("\n", "") for element in head_elements]
+        heads = [re.sub(r"\s", "", element.get_text()) for element in head_elements]
+        del heads[11]
+        heads.append("horse_id")
+        heads.append("jockey_id")
         horse_elements = self.soup.select(self.horse_locator)
         horse_list = []
         for horse_element in horse_elements:
-            horse = HorseShutuba(horse_element, heads.copy())
-            horse_list.append(horse)
+            # .HorseList tr からtdを検索
+            td_element_list = horse_element.select("td")
+            values = [re.sub(r"\s", "", element.get_text()) for element in td_element_list]
+            horse_id = self.__get_horse_id(td_element_list[3])
+            jockey_id = self.__get_jockey_id(td_element_list[6])
+            values.append(horse_id)
+            values.append(jockey_id)
+            horse_list.append(dict(zip(heads, values)))
         return horse_list
     
--- scrape_horse.py
+++ scrape_horse.py
@@ -10,56 +10,60 @@ import time
 import sys
 import datetime
 
-def main():
-    if len(sys.argv) < 4:
-        raise Exception("引数が足りません")
-    root_path = "./data/results"
-    year_start = int(sys.argv[1])
-    year_end = int(sys.argv[2])
-    place = int(sys.argv[3])
-    now = datetime.datetime.now()
+def scrape_horse(horse_id_list):
+    driver = webdriver.Chrome()
+    try:
+        root_path = "./data/horse/race_history"
+        if not os.path.exists(root_path):
+            os.makedirs(root_path)
 
-    if year_start < 2011:
+        for horse_id in horse_id_list:
+            print(horse_id)
+            year = str(horse_id)[0:4]
+            folder = f"{root_path}"
+            if not os.path.exists(folder):
+                os.makedirs(folder)
+            file_path = f"{folder}/{horse_id}.csv"
+            driver.get(f"https://db.netkeiba.com/horse/{horse_id}")
+            horse_page = horse.HorsePage(driver)
+            race_history = horse_page.get_race_history()
+            df = pd.DataFrame(race_history)
+            df["horse_id"] = horse_id
+            df["horse_title"] = horse_page.get_horse_title()
+            if os.path.exists(file_path):
+                df_b = pd.read_csv(file_path, index_col=0)
+                df_b = df_b.append(df)
+                df_b.drop_duplicates(inplace=True)
+                df_b.to_csv(file_path)
+            else:
+                df.to_csv(file_path)
+            time.sleep(1)
+    finally:
+        driver.close()
+
+def scrape_horse_in_year_place(year,place):
+    now = datetime.datetime.now()
+    if year < 2011:
         raise Exception("2011年より前は対応していません")
-    elif year_start > year_end:
-        raise Exception("終了年には開始年より大きな値を設定してください")
-    elif year_end > now.year:
+    elif year > now.year:
         raise Exception("未来の年は入力できません")
     
     if not place in [e.value for e in const.Place]:
         raise Exception("有効なレース場idではありません")
-    root_path = "./data/horse/race_history"
-    if not os.path.exists(root_path):
-        os.makedirs(root_path)
+    driver = webdriver.Chrome()
     try:
-        for place in [e.value for e in const.Place]:
-            for year in range(year_start, year_end + 1):
-                results_path = f"./data/results/{place:02}/{year}_all.csv"
-                df_results = pd.read_csv(results_path, index_col=0)
-                horse_id_list = df_results["horse_id"].unique()
-                driver = webdriver.Chrome()
-                for horse_id in horse_id_list:
-                    year = str(horse_id)[0:4]
-                    file_path = f"{root_path}/{year}_all.csv"
-                    print(horse_id)
-                    driver.get(f"https://db.netkeiba.com/horse/{horse_id}")
-                    horse_page = horse.HorsePage(driver)
-                    race_history = horse_page.get_race_history()
-                    df = pd.DataFrame(race_history)
-                    df["horse_id"] = horse_id
-                    df["horse_title"] = horse_page.get_horse_title()
-                    if os.path.exists(file_path):
-                        df_b = pd.read_csv(file_path, index_col=0)
-                        df_b = df_b.append(df)
-                        df_b.drop_duplicates(inplace=True)
-                        df_b.to_csv(file_path)
-                    else:
-                        df.to_csv(file_path)
-                    time.sleep(1)
+        results_path = f"./data/results/{place:02}/{year}_all.csv"
+        df_results = pd.read_csv(results_path, index_col=0)
+        horse_id_list = df_results["horse_id"].unique()
+        scrape_horse(driver, horse_id_list)
 
     finally:
         driver.close()
 
 
+
 if __name__ == "__main__":
-    main()
+    path = "./tests/shutuba.csv"
+    df = pd.read_csv(path, index_col=0)
+    horse_id_list = df["horse_id"].unique()
+    scrape_horse(horse_id_list)
